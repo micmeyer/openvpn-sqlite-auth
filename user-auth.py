cf4806219d3ad5a44c86610cf8796cafbeac822c
Michael Meyer <meyer711@gmail.com>
2019-04-05 19:33:13 +0100

    Improved logging

--- user-auth.py
+++ user-auth.py
@@ -8,7 +8,7 @@ from config import DB_PATH, HASH_ALGORITHM
 
 # Read username and password from via-file
 filename = sys.argv[1]
-print("[auth-sqlite] filename: " + filename)
+print("[auth-sqlite] filename: " + filename, file=sys.stdout)
 fp = open(filename)
 data = fp.readlines()
 fp.close()
@@ -21,11 +21,13 @@ cursor = conn.cursor()
 
 cursor.execute('SELECT * FROM users WHERE username = ?;', (username,))
 result = cursor.fetchone()
+
 if result is None:
     print("[auth-sqlite] unknown user: " + username, file=sys.stderr)
     sys.exit(1)
-username, password = result
-if hash_func(password.encode("utf-8")).hexdigest() != password:
+
+if hash_func(password.encode("utf-8")).hexdigest() != result[1]:
     print("[auth-sqlite] wrong password, username=" + username, file=sys.stderr)
     sys.exit(1)
+
 sys.exit(0)
